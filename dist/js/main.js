148e5a4e7e230bae0cb7fc4665556c08860dfc79
ryanwolhuter <dev@ryanwolhuter.com>
2019-05-15 14:36:20 +0200

    Add music section with slider

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,12 +2,44 @@ window.addEventListener('DOMContentLoaded', event => {
   particlesJS.load('particles-js', 'js/particlesjs-config.json', function () {
     console.log('callback - particles.js config loaded');
   });  
+  showSlides(slideIndex);
 });
 
-function handleResize() {
-  const doc = document.documentElement;
-  let height = doc.scrollHeight;
-  let width = doc.clientWidth;
-  let background = document.querySelector('particles-js-canvas-el');
-  let context = background.getContext('2d');
+var slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('slide');
+  var dots = document.getElementsByClassName('dot');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+
